fb594990a570be85df0eebe2e8b3e4feb85e5f54
Hamza Butt <40515094+HamzaButt-98@users.noreply.github.com>
2021-01-02 11:24:38 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# essexwhatson
+
+This project is a flutter application to display what's on events on essex website.
---
+++ android/app/src/main/kotlin/com/mobile/essexwhatson/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mobile.essexwhatson
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
